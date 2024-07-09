8e9222312d24614e25958c047e65ddbd631f23e5
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-07-09 10:08:04 +0200

    Handling events and setting state

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-    render() {
-      return (
-        <form className="store-selector">
-          <h2>Please Enter a Store</h2>
-          <input type="text" required placeholder="Store Name" /> 
-          <button tyoe="submit">Visit Store →</button>
-        </form> 
+  
+  myInput = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    //get the text from the input
+    const storeName = this.myInput.current.value;
+    //change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`); //this is a prop that is passed down from the Router because it is a child of the Router
+  }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter a Store</h2>
+        <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/> 
+        <button tyoe="submit">Visit Store →</button>
+      </form> 
     )  
    }
   }
