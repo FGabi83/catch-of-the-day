1b9d12beb986c04494e7719f30d07cfb3066c59c
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-07-09 10:17:27 +0200

    loading sample data into state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -15,7 +16,9 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     // set the new fishes object to state
     this.setState({ fishes });//same as fishes: fishes
-    
+  };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
   };
   render() {
     return (
@@ -24,7 +27,7 @@ class App extends React.Component {
          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div> 
   )  
  }
